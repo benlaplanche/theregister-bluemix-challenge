9c0c027cc65a6fec9b36f4458ae26c401744ab9b
Ben Laplanche <ben@laplanche.co.uk>
2015-07-25 18:08:28 +0100

    WIP to read from a passed in file path

--- factorials/main.go
+++ factorials/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	stringInput := os.Args[1]
-	intInput, _ := strconv.ParseInt(stringInput, 10, 0)
 
-	output := factorial(intInput)
-	fmt.Println(output)
+	input := os.Args[1]
+	file, err := os.Open(input)
+
+	if check(err) == false {
+		// looks like its a file path input
+		defer file.Close()
+
+		reader := bufio.NewReader(file)
+		scanner := bufio.NewScanner(reader)
+
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	} else if check(err) == true {
+		// lets see if its a number string input
+		intInput := stringToInt(input)
+
+		output := factorial(intInput)
+		fmt.Println(output)
+
+	} else if len(os.Args) == 1 {
+		// no input values
+		fmt.Println("Please provide either a file path or a number input")
+	}
+
 }
 
 func factorial(number int64) (factor int64) {
@@ -24,12 +46,23 @@ func factorial(number int64) (factor int64) {
 		var counter int64 = 1
 
 		for i := counter; i <= number; i++ {
-			fmt.Println(factor)
-			fmt.Println(i)
 			factor = factor * i
-
 		}
+		return
 	}
 
-	return factor
+	return
+}
+
+func stringToInt(input string) int64 {
+	result, _ := strconv.ParseInt(input, 10, 0)
+	return result
+}
+
+func check(e error) bool {
+	if e != nil {
+		// encountered an error
+		return true
+	}
+	return false
 }
