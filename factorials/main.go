43828586b7efa603260caa9bfa7825d5e229684c
Ben Laplanche <ben@laplanche.co.uk>
2015-07-25 17:52:22 +0100

    passing tests

--- factorials/main.go
+++ factorials/main.go
@@ -15,8 +15,21 @@ func main() {
 }
 
 func factorial(number int64) (factor int64) {
+	factor = 1
+
 	if number == 1 {
-		return 1
+		return
+	} else {
+
+		var counter int64 = 1
+
+		for i := counter; i <= number; i++ {
+			fmt.Println(factor)
+			fmt.Println(i)
+			factor = factor * i
+
+		}
 	}
-	return 0
+
+	return factor
 }
